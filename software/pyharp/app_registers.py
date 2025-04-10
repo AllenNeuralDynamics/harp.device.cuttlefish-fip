97c417ce9ec617979bace1fc1dc43dedea5dcd87
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2025-04-10 14:31:57 -0700

    add first pyharp test

---
+++ software/pyharp/app_registers.py
@@ -0,0 +1,20 @@
+"""App registers for the cuttlefish-fip controller."""
+from enum import IntEnum
+
+
+class AppRegs(IntEnum):
+    EnableTaskSchedule = 32
+    AddLaserTask = 33
+    RemoveLaserTask = 34
+    RemoveAllLaserTasks = 35
+    LaserTaskcount = 36
+    RisingEdgeEvent = 37
+
+    ReconfigureLaserTask0 = 38
+    ReconfigureLaserTask1 = 39
+    ReconfigureLaserTask2 = 40
+    ReconfigureLaserTask3 = 41
+    ReconfigureLaserTask4 = 42
+    ReconfigureLaserTask5 = 43
+    ReconfigureLaserTask6 = 44
+    ReconfigureLaserTask7 = 45
