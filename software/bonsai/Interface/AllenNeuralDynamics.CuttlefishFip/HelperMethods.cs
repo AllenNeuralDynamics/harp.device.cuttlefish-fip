870a7c45bb824f53397f03432f0c7feb64214b0c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-04-18 10:04:08 -0700

    Add first draft of bonsai interface

---
+++ software/bonsai/Interface/AllenNeuralDynamics.CuttlefishFip/HelperMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace AllenNeuralDynamics.CuttlefishFip
+{
+    [Description("Class for helper methods.")]
+    public class HelperMethods
+    {
+        [Description("Converts a struct to its byte array representation.")]
+        internal static unsafe byte[] StructToByteArray(TaskPayload value)
+        {
+            var payload = new[] { value };
+            byte[] bytes = new byte[payload.Length * sizeof(TaskPayload)];
+
+            fixed (void* src = payload)
+            {
+                fixed (void* dst = bytes)
+                {
+                    Buffer.MemoryCopy(src, dst, bytes.Length, bytes.Length);
+                }
+            }
+            return bytes;
+        }
+
+        internal static unsafe TaskPayload ByteArrayToStruct(byte[] value)
+        {
+            TaskPayload[] newArray = new TaskPayload[1];
+            Buffer.BlockCopy(value, 0, newArray, 0, value.Length);
+            return newArray[0];
+        }
+
+    }
+    [Description("Struct of settings for the task.")]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TaskPayload
+    {
+        public uint pwmPort;
+        public float dutyCycle;
+        public float frequency;
+        public uint triggerPorts;
+        public byte eventsEnabled;
+        public byte isMuted;
+        public uint delta1;
+        public uint delta2;
+        public uint delta3;
+        public uint delta4;
+    }
+}
--- software/bonsai/Interface/AllenNeuralDynamics.CuttlefishFip/Properties/AssemblyInfo.cs
+++ software/bonsai/Interface/AllenNeuralDynamics.CuttlefishFip/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: XmlNamespacePrefix("clr-namespace:AllenNeuralDynamics.Cuttlefish", null)]
+[assembly: XmlNamespacePrefix("clr-namespace:AllenNeuralDynamics.CuttlefishFip", null)]
 [assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Daq")]
